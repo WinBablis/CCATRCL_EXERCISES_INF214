c4b6b6a507c666c9306498a4803ea669ba376e66
WinBablis <112592753+WinBablis@users.noreply.github.com>
2022-09-06 13:24:59 +0800

    Update Weapon.java

--- exercise_1/Weapon.java
+++ exercise_1/Weapon.java
@@ -1,1 +1,26 @@
+weapon shaper = new weapon();
 
+shaper.name = "Summit Shaper";
+shaper.damage = 46;
+shaper.weight = 0.3;
+shaper.rarity = "Five Star";
+shaper.description = "Increases shield strength by 40%";
+shaper.Weapon_1();
+
+weapon spine = new weapon();
+
+spine.name = "Serpent Shaper";
+spine.damage = 42;
+spine.weight = 1.8;
+spine.rarity = "Four Star";
+spine.description = "This effect has a maximun of five stacks and will not be reset if the character leaves the field";
+spine.Weapon_1();
+
+weapon harp = new weapon();
+
+harp.name = "Skyward Harp";
+harp.damage = 48;
+harp.weight = 2.5;
+harp.rarity = "Five Star";
+harp.description = "Increases critical damage by 20%";
+harp.Weapon_1();
